8fc00e629f1be34a2049175b9eae639152c2a8ec
Nassime <nassimeelkamari2002@gmail.com>
2024-04-17 12:02:10 +0100

    final commit

---
+++ src/components/root/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../navigation';
+import { Outlet } from 'react-router-dom';
+const Root = () => {
+    return (
+        <>
+            <Navigation/>
+            <Outlet/>
+        </>
+    );
+};
+
+export default Root;
---
+++ src/components/search/index.js
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+// Import createSearchParams
+// Import useNavigate
+
+import { useNavigate , createSearchParams} from 'react-router-dom';
+
+const Search = () => {
+
+  // get navigate function
+  const navigate = useNavigate();
+
+  const searchInputRef = useRef();
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+
+    const searchQuery = {
+      name: searchInputRef.current.value
+    }
+
+    // use createSearchParams 
+    const query = createSearchParams(searchQuery);
+
+
+    navigate({
+      pathname: '/search',
+      search: `?${query}`
+    })  
+    };
+
+  return (
+    <form onSubmit={onSearchHandler} className="search-form">
+      <input type="text" className="search" ref={searchInputRef} />
+      <button type="submit" className="search-button">
+        🔎
+      </button>
+    </form>
+  );
+};
+
+export default Search;
